c007747908044665329c9515d5f8b86689bfef69
yap cheah shen <yapcheahshen@gmail.com>
2023-11-06 21:02:45 +0800

    add wuci

---
+++ src/appstore.js
@@ -0,0 +1,18 @@
+export * from 'accelon23/src/store.js';
+import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
+
+_ACC23.CacheName='v1::agm',
+_ACC23.AppVer='23.10.31';
+_ACC23.AppName='agm'
+_ACC23.AppTitle='我的應用'
+_ACC23.allptks=['agm','agm-wuci'];
+_ACC23.selectedptks=['agm']
+_ACC23.activeptk='agm'
+_ACC23.ptkinfo={
+    'agm':"阿含",
+    'agm-wuci':"悟慈",
+}
+_ACC23.favorites=[];
+_ACC23.MAXFAVORITE=100;
+_ACC23.ITEMPERPAGE=5;
+export const ACC23 = _ACC23;
