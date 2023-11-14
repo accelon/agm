37b4cf4859c71d62ce64ad82413c69e5296edce6
yap cheah shen <yapcheahshen@gmail.com>
2023-11-14 11:16:00 +0800

    bump version

--- src/appstore.js
+++ src/appstore.js
@@ -2,7 +2,7 @@ export * from 'accelon23/src/store.js';
 import {ACC23 as _ACC23} from 'accelon23/src/appconst.js'
 
 _ACC23.CacheName='v1::agm',
-_ACC23.AppVer='23.10.31';
+_ACC23.AppVer='23.11.13';
 _ACC23.AppName='agm'
 _ACC23.AppTitle='我的應用'
 _ACC23.allptks=['agm','agm-wuci'];
